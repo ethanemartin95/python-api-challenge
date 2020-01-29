7a913ce260d80f95555b158258078882403cf625
Ethan Martin <ethan_martin95@Ethans-MacBook-Pro-2.local>
2020-01-29 14:06:47 -0500

    API HW

---
+++ VacationPy/api_keys.py
@@ -0,0 +1,6 @@
+# OpenWeatherMap API Key
+weather_api_key = "YOUR KEY HERE!"
+
+# Google API Key
+g_key = "AIzaSyAzrS1tNPmwquiyu3geCs6ASiTRvy6p7Ls"
+
---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "bc45a521e0dab7993efb9110dd351cb5"
+
+# Google API Key
+g_key = "YOUR KEY HERE!"
